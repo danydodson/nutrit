808e4324abe0381d583db7e6a6bc76027f1f4d2f
danydodson <danydodson@gmail.com>
2019-06-19 16:55:39 -0500

    env vars

--- src/js/heroku.js
+++ src/js/heroku.js
@@ -1,2 +1,4 @@
-export const proxy = process.env.PROXY
-export const key = process.env.KEY
+export const proxy = 'https://cors-anywhere.herokuapp.com/'
+export const key = '869e1dae56007572c54e27fe00d62f90'
+//export const proxy = process.env.PROXY
+//export const key = process.env.KEY
